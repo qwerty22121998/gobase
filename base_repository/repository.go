a25290197f8098d3992127a0cfdb85bef6fdea02
vuhk <qwerty22121998@gmail.com>
2024-11-22 13:47:03 +0000

    feat(query): simple query

--- base_repository/repository.go
+++ base_repository/repository.go
@@ -6,7 +6,7 @@ import (
 	"github.com/qwerty22121998/gobase/base_util"
 	"github.com/qwerty22121998/gobase/pagination"
 	"github.com/qwerty22121998/gobase/preload"
-	"github.com/qwerty22121998/gobase/query/complex_query"
+	"github.com/qwerty22121998/gobase/query"
 	"gorm.io/gorm"
 )
 
@@ -20,10 +20,10 @@ type IRepository[T base_model.IModel] interface {
 	Save(ctx context.Context, data T) error
 	Delete(ctx context.Context, data T) error
 	FindByID(ctx context.Context, id uint) (T, error)
-	FindFirst(ctx context.Context, q complex_query.Query, preloads ...preload.Opt) (T, error)
-	FindMany(ctx context.Context, q complex_query.Query, p *pagination.Pagination, preloads ...preload.Opt) ([]T, error)
-	FindAll(ctx context.Context, q complex_query.Query, preloads ...preload.Opt) ([]T, error)
-	Count(ctx context.Context, q complex_query.Query, preloads ...preload.Opt) (int64, error)
+	FindFirst(ctx context.Context, q query.Query, preloads ...preload.Opt) (T, error)
+	FindMany(ctx context.Context, q query.Query, p *pagination.Pagination, preloads ...preload.Opt) ([]T, error)
+	FindAll(ctx context.Context, q query.Query, preloads ...preload.Opt) ([]T, error)
+	Count(ctx context.Context, q query.Query, preloads ...preload.Opt) (int64, error)
 	BeginTx(ctx context.Context, fn func(ctx context.Context) error) error
 }
 
@@ -71,20 +71,21 @@ func (r *Repository[T]) FindByID(ctx context.Context, id uint) (T, error) {
 	return result, err
 }
 
-func (r *Repository[T]) FindFirst(ctx context.Context, q complex_query.Query, preloads ...preload.Opt) (T, error) {
+func (r *Repository[T]) FindFirst(ctx context.Context, q query.Query, preloads ...preload.Opt) (T, error) {
 	var result T
 	zero := new(T)
 	db := r.DB(ctx).Model(zero)
-	db = db.Scopes(q)
+	db = q.Apply(db)
 	db = preload.Group(preloads...).Apply(db)
 	err := db.First(&result).Error
 	return result, err
 }
 
-func (r *Repository[T]) Count(ctx context.Context, q complex_query.Query, preloads ...preload.Opt) (int64, error) {
+func (r *Repository[T]) Count(ctx context.Context, q query.Query, preloads ...preload.Opt) (int64, error) {
 	var model T
 	db := r.DB(ctx).Model(model)
-	db = preload.Group(preloads...).Apply(db).Scopes(q)
+	db = preload.Group(preloads...).Apply(db)
+	db = q.Apply(db)
 	var total int64
 	if err := db.Count(&total).Error; err != nil {
 		return 0, err
@@ -92,11 +93,11 @@ func (r *Repository[T]) Count(ctx context.Context, q complex_query.Query, preloa
 	return total, nil
 }
 
-func (r *Repository[T]) FindAll(ctx context.Context, q complex_query.Query, preloads ...preload.Opt) ([]T, error) {
+func (r *Repository[T]) FindAll(ctx context.Context, q query.Query, preloads ...preload.Opt) ([]T, error) {
 	res := make([]T, 0)
 	zero := new(T)
 	db := r.DB(ctx).Model(zero)
-	db = db.Scopes(q)
+	db = q.Apply(db)
 	db = preload.Group(preloads...).Apply(db)
 	if err := db.Find(&res).Error; err != nil {
 		return nil, err
@@ -104,12 +105,12 @@ func (r *Repository[T]) FindAll(ctx context.Context, q complex_query.Query, prel
 	return res, nil
 }
 
-func (r *Repository[T]) FindMany(ctx context.Context, q complex_query.Query, p *pagination.Pagination, preloads ...preload.Opt) ([]T, error) {
+func (r *Repository[T]) FindMany(ctx context.Context, q query.Query, p *pagination.Pagination, preloads ...preload.Opt) ([]T, error) {
 	p.Correct()
 	res := make([]T, 0)
 	zero := new(T)
 	db := r.DB(ctx).Model(zero)
-	db = db.Scopes(q)
+	db = q.Apply(db)
 	db = preload.Group(preloads...).Apply(db)
 	var total int64
 	if err := db.Count(&total).Error; err != nil {
